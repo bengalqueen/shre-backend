57bed84532eff91708b2a6d0101e01fd26544277
shreyadutta2505@gmail.com <shreyadutta2505@gmail.com>
2025-08-26 13:26:54 +0530

    add register controller and bug fixes

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,7 +1,7 @@
 class ApiError extends Error {
     constructor(
         statuseCode,
-        message: "Something went wrong",
+        message= "Something went wrong",
         errors = [],
         stack = ""
     ){
