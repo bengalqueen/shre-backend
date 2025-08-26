b7f125bc5e4ea8c215a3a1cba69b655f6d6fcc46
shreyadutta2505@gmail.com <shreyadutta2505@gmail.com>
2025-08-26 16:27:31 +0530

    fix all register controller bugs

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import userRouter from "./routes/user.routes.js";
 const app = express()
  
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
 // Middleware
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,10 +16,21 @@ const uploadOnCloudinary = async (localFilePath) => {
         })
         // file has been uploaded successfully
         console.log("file is uploaded on cloudinary", response.url);
+        
+        if (fs.existsSync(localFilePath)) {
+            fs.unlinkSync(localFilePath);
+        }
         return response;
+
     } catch (error) {
-        fs.unlinkSync(localFilePath)  //remove the locally saved temporary file as the upload 
+        console.error("Cloudinary upload error:", error);
+
+        // Safely remove temp file even if upload fails
+        if (localFilePath && fs.existsSync(localFilePath)) {
+            fs.unlinkSync(localFilePath);  //remove the locally saved temporary file as the upload 
         // operation got failed
+        }
+
         return null;
     }
 }
